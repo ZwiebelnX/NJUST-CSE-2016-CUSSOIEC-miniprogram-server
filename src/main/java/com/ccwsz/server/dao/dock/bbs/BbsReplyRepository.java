08c22d7f437de4185fed703e9849e2d2dbed96f3
Sun Mingming <274303275@qq.com>
2019-03-04 19:40:35 +0800

    分支测试2

--- src/main/java/com/ccwsz/server/dao/dock/bbs/BbsReplyRepository.java
+++ src/main/java/com/ccwsz/server/dao/dock/bbs/BbsReplyRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface BbsReplyRepository extends CrudRepository<BbsReplyEntity,Integer> {
+    //
     List<BbsReplyEntity> findByTopicId(Pageable pageable,long topicId);
 }
