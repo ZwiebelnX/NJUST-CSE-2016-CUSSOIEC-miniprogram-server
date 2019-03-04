3ab2d76b2e15e0694a6f35240a78b9d012340351
Chen Sicong <sicong.chen@163.com>
2019-03-04 19:31:29 +0800

    分支测试1

--- src/main/java/com/ccwsz/server/dao/dock/info/InfoStreamBbsReplyRepository.java
+++ src/main/java/com/ccwsz/server/dao/dock/info/InfoStreamBbsReplyRepository.java
@@ -2,6 +2,7 @@ package com.ccwsz.server.dao.dock.info;
 
 import com.ccwsz.server.dao.entity.InfoStreamBbsReplyEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface InfoStreamBbsReplyRepository extends CrudRepository<InfoStreamBbsReplyEntity, Integer> {
+public interface InfoStreamBbsReplyRepository extends PagingAndSortingRepository<InfoStreamBbsReplyEntity, Integer> {
 }
